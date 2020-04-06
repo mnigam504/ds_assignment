eeb566b250e9f6f4176c34ebaad0b692141a908d
mnigam504 <55240295+mnigam504@users.noreply.github.com>
2020-04-06 14:27:04 +0530

    Add files via upload

---
+++ inorder.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+struct node* create(int data)
+{
+  struct node* key= (struct node*)malloc(sizeof(struct node));
+  key->data = data;
+  key->left = NULL;
+  key->right = NULL;
+  return(key);
+}
+void inorder(struct node *root)
+{
+    if(root!=NULL)
+    {
+        inorder(root->left);
+        printf("%d ",root->data);
+        inorder(root->right);
+    }
+}
+int main()
+{
+  struct node *root = create(10);
+  root->left = create(20);
+  root->right = create(30);
+  root->left->left  = create(40);
+  root->right->left=create(50);
+  root->right->right=create(60);
+  inorder(root);
+  getchar();
+  return 0;
+}
